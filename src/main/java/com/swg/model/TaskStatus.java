5e611fbf2cd4a2879fa58b7829447b8cb7179c06
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2026-03-07 20:19:34 +0530

    feat: pending, assign, done

--- src/main/java/com/swg/model/TaskStatus.java
+++ src/main/java/com/swg/model/TaskStatus.java
@@ -1,4 +1,19 @@
 package com.swg.model;
 
+import org.springframework.scheduling.config.Task;
+
 public enum TaskStatus {
+
+
+    PENDING(  "PENDING"),
+
+    ASSIGNED("ASSIGNED"),
+
+    DONE("DONE");
+
+
+    TaskStatus(String done ){
+
+    }
+
 }
