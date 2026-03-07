f59831b91e2b7ed48be2cd4ad6ccade4ccc1553c
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2026-03-07 20:46:03 +0530

    feat: interface taskservice

--- src/main/java/com/swg/service/TaskService.java
+++ src/main/java/com/swg/service/TaskService.java
@@ -1,4 +1,26 @@
 package com.swg.service;
 
-public class TaskService {
+import com.swg.model.Task;
+import com.swg.model.TaskStatus;
+
+import java.util.List;
+
+public interface TaskService {
+
+    Task createTask (Task task, String requesterRole) throws Exception;
+
+    Task getTaskById (Long id) throws Exception;
+
+    List<Task> getAllTask (TaskStatus status);
+
+    Task updateTask ( Long id, Task updatedTask, Long userId) throws  Exception;
+
+    void deleteTask(Long id);
+
+    Task assignedToUser(Long userId, Long taskId)throws Exception;
+
+    List<Task>assignedUsersTask(Long userId, TaskStatus status);
+
+    Task completeTask(Long taskId) throws Exception;
+
 }
