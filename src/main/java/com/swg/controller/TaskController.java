bcad7972cb9a2ee124ebfe9e27787cf6cc4bd9dd
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2026-03-11 08:48:19 +0530

    feat: mapping createTask, getTaskById

--- src/main/java/com/swg/controller/TaskController.java
+++ src/main/java/com/swg/controller/TaskController.java
@@ -1,6 +1,46 @@
 package com.swg.controller;
 
+
+import com.swg.model.Task;
+import com.swg.model.UserDto;
+import com.swg.service.TaskService;
+import com.swg.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/tasks")
 public class TaskController {
+
+    @Autowired
+    private TaskService taskService;
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<Task> createTask(@RequestBody Task task,
+                                          @RequestHeader("Authorization") String jwt) throws Exception {
+          UserDto user = userService.getUserProfile(jwt);
+
+          Task createdTask = taskService.createTask(task, user.getRole());
+
+        return new ResponseEntity<>(createdTask, HttpStatus.CREATED);
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> getTaskById(@PathVariable Long id,
+                                           @RequestHeader("Authorization") String jwt) throws Exception {
+        UserDto user = userService.getUserProfile(jwt);
+
+        Task task = taskService.getTaskById(id);
+
+        return new ResponseEntity<>(task, HttpStatus.CREATED);
+    }
 
 
 }
