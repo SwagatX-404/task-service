9f82959fc9276bd9ae9c39d591683f1f4e11a90d
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2026-03-11 08:12:46 +0530

    feat: interface user service with feign client

--- src/main/java/com/swg/service/UserService.java
+++ src/main/java/com/swg/service/UserService.java
@@ -1,4 +1,10 @@
 package com.swg.service;
 
+
+import org.springframework.cloud.openfeign.FeignClient;
+
+@FeignClient(name="USER_SERVICE", url="http://localhost:5001")
 public interface UserService {
+
+
 }
