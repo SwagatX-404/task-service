1c0db5f813f84d8f657504a6c783e0b55a2a6132
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2026-03-14 16:50:50 +0530

    feat: getmapping /tasks

--- src/main/java/com/swg/controller/HomeController.java
+++ src/main/java/com/swg/controller/HomeController.java
@@ -1,9 +1,23 @@
 package com.swg.controller;
 
+import com.swg.model.Task;
+import com.swg.model.TaskStatus;
+import com.swg.model.UserDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class HomeController {
 
-    
+    @GetMapping("/tasks")
+    public ResponseEntity<String> getAssignedUsersTask(){
+
+        return new ResponseEntity<>("Welcome to Task Service", HttpStatus.OK);
+    }
 }
